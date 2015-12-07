eaa1403ed8ddb884616ffb24819c7e95d6f20084
pebcak <pebcak@rogueai.com>
2015-12-07 21:03:33 +0000

    Improved unit test coverage

--- src/test/java/com/lp/test/parser/impl/DestinationStaxStreamParserTest.java
+++ src/test/java/com/lp/test/parser/impl/DestinationStaxStreamParserTest.java
@@ -1,8 +1,12 @@
 package com.lp.test.parser.impl;
 
 import com.lp.test.model.Destination;
+import com.lp.test.parser.exception.ParseException;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.InputStream;
 import java.util.Map;
@@ -12,11 +16,34 @@ import java.util.Map;
  */
 public class DestinationStaxStreamParserTest {
 
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void test_parse_empty() throws Exception {
+        DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
+        InputStream is = getClass().getResourceAsStream("/xml/destinations-empty.xml");
+        exception.expect(ParseException.class);
+        parser.parse(is);
+    }
+
+    @Test
+    public void test_parse_malformed() throws Exception {
+        DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
+        InputStream is = getClass().getResourceAsStream("/xml/destinations-malformed.xml");
+        exception.expect(ParseException.class);
+        parser.parse(is);
+    }
+
     @Test
     public void test_parse_original() throws Exception {
         DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
         InputStream is = getClass().getResourceAsStream("/xml/destinations-orig.xml");
         Map<Integer, Destination> result = parser.parse(is);
+        Assert.assertNotNull(result);
         Assert.assertEquals(24, result.size());
     }
 
@@ -25,6 +52,7 @@ public class DestinationStaxStreamParserTest {
         DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
         InputStream is = getClass().getResourceAsStream("/xml/destinations-min.xml");
         Map<Integer, Destination> result = parser.parse(is);
+        Assert.assertNotNull(result);
         Assert.assertEquals(1, result.size());
         Assert.assertTrue(result.containsKey(355064));
         Destination destination = result.get(355064);
@@ -33,6 +61,24 @@ public class DestinationStaxStreamParserTest {
         Assert.assertNotNull(destination.getIntroductory());
         Assert.assertNotNull(destination.getIntroductory().getIntroduction());
         Assert.assertNotNull(destination.getIntroductory().getIntroduction().getOverview());
+    }
+
+    @Test
+    public void test_parse_inline() throws Exception {
+        DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
+        InputStream is = getClass().getResourceAsStream("/xml/destinations-inline.xml");
+        Map<Integer, Destination> result = parser.parse(is);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void test_parse_comments() throws Exception {
+        DestinationStaxStreamParser parser = new DestinationStaxStreamParser();
+        InputStream is = getClass().getResourceAsStream("/xml/destinations-comments.xml");
+        Map<Integer, Destination> result = parser.parse(is);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(2, result.size());
     }
 
 }
--- src/test/java/com/lp/test/parser/impl/TaxonomyStaxStreamParserTest.java
+++ src/test/java/com/lp/test/parser/impl/TaxonomyStaxStreamParserTest.java
@@ -1,34 +1,92 @@
 package com.lp.test.parser.impl;
 
 import com.lp.test.model.Destination;
+import com.lp.test.model.Destinations;
+import com.lp.test.model.Introductory;
 import com.lp.test.model.Node;
+import com.lp.test.model.Taxonomies;
+import com.lp.test.model.Taxonomy;
+import com.sun.xml.internal.bind.marshaller.CharacterEscapeHandler;
 import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 import java.util.Map;
 
 /**
  * @author Massimo Zugno <d3k41n@gmail.com>
  */
 public class TaxonomyStaxStreamParserTest {
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
     @Test
     public void test_parse_original() throws Exception {
-        TaxonomyStaxStreamParser parser = new TaxonomyStaxStreamParser();
-        InputStream is = getClass().getResourceAsStream("/xml/taxonomy-orig.xml");
-        Map<Integer, Node> result = parser.parse(is);
+        Map<Integer, Node> result = parse("/xml/taxonomy-orig.xml");
         Assert.assertEquals(24, result.size());
     }
 
     @Test
     public void test_parse_min() throws Exception {
-        TaxonomyStaxStreamParser parser = new TaxonomyStaxStreamParser();
-        InputStream is = getClass().getResourceAsStream("/xml/taxonomy-min.xml");
-        Map<Integer, Node> result = parser.parse(is);
+        Map<Integer, Node> result = parse("/xml/taxonomy-min.xml");
+
         Assert.assertEquals(1, result.size());
         Assert.assertTrue(result.containsKey(355064));
         Node node = result.get(355064);
         Assert.assertEquals(355064l, node.getId());
         Assert.assertEquals("Africa", node.getName());
     }
+
+    @Test
+    public void test_parse_multiline() throws Exception {
+        Map<Integer, Node> result = parse("/xml/taxonomy-multiline.xml");
+
+        Assert.assertEquals(1, result.size());
+        Assert.assertTrue(result.containsKey(1));
+        Node node = result.get(1);
+        Assert.assertEquals("Africa", node.getName());
+    }
+
+    @Test
+    public void test_parse_comments_1() throws Exception {
+        Map<Integer, Node> result = parse("/xml/taxonomy-comments.xml");
+
+        Assert.assertEquals(1, result.size());
+        Assert.assertTrue(result.containsKey(1));
+        Node node = result.get(1);
+        Assert.assertEquals("Africa", node.getName());
+    }
+
+    @Test
+    public void test_parse_comments_2() throws Exception {
+        Map<Integer, Node> result = parse("/xml/taxonomy-comments-2.xml");
+
+        Assert.assertEquals(1, result.size());
+        Assert.assertTrue(result.containsKey(1));
+        Node node = result.get(1);
+        Assert.assertEquals("South Africa", node.getName());
+    }
+
+    @Test
+    public void test_parse_inline() throws Exception {
+        Map<Integer, Node> result = parse("/xml/taxonomy-inline.xml");
+
+        Assert.assertEquals(2, result.size());
+    }
+
+    private Map<Integer, Node> parse(String filePath) throws Exception {
+        TaxonomyStaxStreamParser parser = new TaxonomyStaxStreamParser();
+        InputStream is = getClass().getResourceAsStream(filePath);
+        return parser.parse(is);
+    }
+
 }
